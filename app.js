34972731466a774756f664ffbee894354f460ef6
Norbert <norbert@Norberts-MacBook-Pro.local>
2020-07-09 22:15:57 -0500

    crazyObject

--- app.js
+++ app.js
@@ -1,25 +1,49 @@
-// ### 2. Take it Easy
-
-// 1. Make an array that holds all of the colors of the rainbow.
-// 2. Write code that will access "blue" from the rainbow array.
-// 3. Make an object that is called your name and holds the information about your favorite food, 
-//a hobby, the name of the town that you live in currently, and your favorite datatype.
-// 4. Write code that will access your hobby from the object that you just created.
+//### 3. Crazy Object!
 
 
-const rainbow = {
-    colors: ['red', 'green', 'yellow','blue']
-}
-console.log(rainbow.colors[3])
-
-
-const norbert = {
-    favFood: 'cheeseBurger',
-    hobby: ['airPlainModels'],
-    currentTown: 'Rockford',
-    favDataType: 'numbers',
-
-}
-console.log(norbert.hobby[0])
-
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+  }
+  
+  console.log(crazyObject.taco[1].salsa[crazyObject.taco[1].salsa.length -1]) //"omg my mouth is burning"
+  console.log(crazyObject.larry.quotes[0])    //"Pretty pretty prettayyyyy good"
+  console.log(crazyObject.larry.characters[2].favourtieHobby) //"Swearing at Larry and Jeff"
+  console.log(crazyObject.larry.nicknames[1])  //"Chicken Teriyaki Boyyyyyy"
+  
+  // Use crazyObject to log the following.
+  
+  //   * "omg my mouth is burning"
+  //   * "Pretty pretty prettayyyyy good"
+  //   * "Swearing at Larry and Jeff"
+  //   * "Chicken Teriyaki Boyyyyyy"
+  //   * The object the contains the name `funkhauser`
+  
 
