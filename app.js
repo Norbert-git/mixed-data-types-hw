3990577e86646b541fc3e0b326efe8765e1bb22d
Norbert <norbert@Norberts-MacBook-Pro.local>
2020-07-09 22:36:21 -0500

    data structures

--- app.js
+++ app.js
@@ -60,4 +60,29 @@
                 ]
 
 
-                
+ 
+// ### 2. Take it Easy
+
+// 1. Make an array that holds all of the colors of the rainbow.
+// 2. Write code that will access "blue" from the rainbow array.
+// 3. Make an object that is called your name and holds the information about your favorite food, 
+//a hobby, the name of the town that you live in currently, and your favorite datatype.
+// 4. Write code that will access your hobby from the object that you just created.
+
+
+const rainbow = {
+    colors: ['red', 'green', 'yellow','blue']
+}
+console.log(rainbow.colors[3])
+
+
+const norbert = {
+    favFood: 'cheeseBurger',
+    hobby: ['airPlainModels'],
+    currentTown: 'Rockford',
+    favDataType: 'numbers',
+
+}
+console.log(norbert.hobby[0])
+
+               
