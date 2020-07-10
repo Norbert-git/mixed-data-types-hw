c893749f4df19842569e7115b0c13d7c864232fe
Norbert <norbert@Norberts-MacBook-Pro.local>
2020-07-09 22:05:24 -0500

    Mixed data

---
+++ app.js
@@ -0,0 +1,55 @@
+// ### 1. Data types
+
+// #### Refresher
+
+// So far we have seen:
+
+// * Strings
+// * Numbers
+// * Booleans
+// * Arrays
+// * Objects
+
+// When these datatypes are combined, we get a **data structure**, for example,
+// an array that contains objects that each contain booleans.
+
+// Suppose you are tasked with creating some software, and it is up to **you**
+// to determine which datatypes and what data structure to use.  For each of the following,
+// write which **data types** you would use to represent the data, and then give a small example of the 
+//**data structure**:
+
+//   * A light switch that can be either on or off.
+//   * A user's email address.
+//   * A spaceship with a hull, laser blasters, tractor beam, and warp drive.
+//   * A list of student names from our class.
+//   * A list of student names from our class, each with a location.
+//   * A list of student names from our class, each with a location and each with a list of favorite tv shows.
+
+
+
+    //lightSwitch: ['on', 'off'],  Booleans
+    const lightSwitch = (on) => {
+        if (input === "1") return On;
+        return Off;
+      };
+      lightSwitch('1')    
+      lightSwitch('0')
+
+      //Students Name: Array
+      const nameOfStudents = ['steve', 'bob', 'isabella', 'James']
+        console.log(nameOfStudents)
+
+      // Students name, location and favorite tv show : object
+      const   studentInfo = {
+               info [
+                 {
+                   name: 
+                   location: 
+                   favoriteTvShow:
+                 }
+                 {
+                    name:
+                    location:
+                    favoriteTvShow:
+                  }
+                ]
