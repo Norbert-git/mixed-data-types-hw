04f1dc50eb5ce0ed1c70e6999dba5340ed4e9f02
Norbert <norbert@Norberts-MacBook-Pro.local>
2020-07-09 22:19:04 -0500

    object ception

--- app.js
+++ app.js
@@ -1,49 +1,25 @@
-//### 3. Crazy Object!
+// ### 4. Object-ception
 
-
-const crazyObject = {
-    taco: [
-      {
-        meat: 'steak',
-        cheese: ['panela', 'queso', 'chihuahua']
-      },
-      {
-        meat: 'chicken',
-        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
-      },
-    ],
-    larry: {
-      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
-      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
-      characters: [
-        {
-          name: "Jeff",
-          occupation: "manager"
-        },
-        {
-          name: "funkhauser",
-          occupation: "tv dude"
-        },
-        {
-          name: "susie",
-          occupation: "jeffs wife",
-          favourtieHobby: "Swearing at Larry and Jeff"
-        },
-      ]
+// With the following object:
+const inception = {
+    reality: {
+        dreamLayer1: {
+            dreamLayer2: {
+                dreamLayer3: {
+                    dreamLayer4: {
+                        dreamLayer5: {
+                            dreamLayer6: {
+                                limbo: "Joseph Gordon Levitt"
+                           }
+                        }
+                    }
+                }
+            }
+        }
     }
   }
+  // Change the value of `limbo` to `null`.
   
-  console.log(crazyObject.taco[1].salsa[crazyObject.taco[1].salsa.length -1]) //"omg my mouth is burning"
-  console.log(crazyObject.larry.quotes[0])    //"Pretty pretty prettayyyyy good"
-  console.log(crazyObject.larry.characters[2].favourtieHobby) //"Swearing at Larry and Jeff"
-  console.log(crazyObject.larry.nicknames[1])  //"Chicken Teriyaki Boyyyyyy"
-  
-  // Use crazyObject to log the following.
-  
-  //   * "omg my mouth is burning"
-  //   * "Pretty pretty prettayyyyy good"
-  //   * "Swearing at Larry and Jeff"
-  //   * "Chicken Teriyaki Boyyyyyy"
-  //   * The object the contains the name `funkhauser`
-  
+  inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo = 'null'
+  console.log(inception.reality.dreamLayer1.dreamLayer2.dreamLayer3.dreamLayer4.dreamLayer5.dreamLayer6.limbo)
 
